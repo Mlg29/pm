3e00a0120303590bd4af26e856378a3d8a4efbb0
mlg29 <lmukhtar29@gmail.com>
2024-06-20 17:23:23 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
