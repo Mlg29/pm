72468ac8c57ebc788cdea614c45bafdec3f8a339
mlg29 <lmukhtar29@gmail.com>
2024-12-27 19:27:09 +0100

    updd

--- src/screens/Games/Football.tsx
+++ src/screens/Games/Football.tsx
@@ -52,19 +52,19 @@ function Football() {
 
   useEffect(() => {
     const payloadUpcoming = {
-      status: "UPCOMING",
+      range: 'd1'
     };
     const payloadLive = {
       range: "live",
     };
     const payloadToday = {
-      date: moment(new Date()).format("YYYY-MM-DD"),
+      date: 'home',
     };
     const payloadTomorrow = {
-      date: tomorrowDate.format("YYYY-MM-DD"),
+      range: 'd2',
     };
     const payloadFinished = {
-      status: "ENDED",
+      range: 'd-1'
     };
 
     dispatch(getFootballFixtures(payloadLive)).then((dd) => {
@@ -73,67 +73,17 @@ function Football() {
     // dispatch(getFootballFixtures(payloadToday)).then((dd) => {
     //   setToday(dd?.payload);
     // });
-    // dispatch(getFootballFixtures(payloadFinished)).then((dd) => {
-    //   setFinished(dd?.payload);
-    // });
-    // dispatch(getFootballFixtures(payloadTomorrow)).then((dd) => {
-    //   setTomorrow(dd?.payload);
-    // });
-    // dispatch(getFootballFixtures(payloadUpcoming)).then((dd) => {
-    //   setUpcoming(dd?.payload);
-    // });
+    dispatch(getFootballFixtures(payloadFinished)).then((dd) => {
+      setFinished(dd?.payload);
+    });
+    dispatch(getFootballFixtures(payloadTomorrow)).then((dd) => {
+      setTomorrow(dd?.payload);
+    });
+    dispatch(getFootballFixtures(payloadUpcoming)).then((dd) => {
+      setUpcoming(dd?.payload);
+    });
   }, []);
 
-  const groupedByLeague = live?.filter((a, i) => i < 10).reduce((acc, current) => {
-    const league = current?.leagueName;
-
-    if (!acc[league]) {
-      acc[league] = [];
-    }
-
-    acc[league].push(current);
-
-    return acc;
-  }, {});
-
-
-  const groupedByUpcomingLeague =  upcoming?.data?.reduce((acc, current) => {
-    const league = current?.leagueName;
-
-    if (!acc[league]) {
-      acc[league] = [];
-    }
-
-    acc[league].push(current);
-
-    return acc;
-  }, {});
-
-
-  const groupedByTodayLeague =  today?.data?.reduce((acc, current) => {
-    const league = current?.leagueName;
-
-    if (!acc[league]) {
-      acc[league] = [];
-    }
-
-    acc[league].push(current);
-
-    return acc;
-  }, {});
-
-
-  const groupedByTomorrowLeague = tomorrow?.data?.reduce((acc, current) => {
-    const league = current?.leagueName;
-
-    if (!acc[league]) {
-      acc[league] = [];
-    }
-
-    acc[league].push(current);
-
-    return acc;
-  }, {});
 
 
 
@@ -238,8 +188,7 @@ function Football() {
      {
       selectedStatus === "Scheduled" ?
       <>
-    
-      {upcoming?.data?.length > 0 && (
+       {upcoming?.length > 0 && (
         <div
           style={{
             display: "flex",
@@ -248,9 +197,10 @@ function Football() {
           }}
         >
           <p style={{ ...FONTS.body6, color: COLORS.gray, margin: "15px 0px" }}>
-       
+            
           </p>
-          {upcoming?.total > 10 && (
+
+          {upcoming?.length > 10 && (
             <p
               style={{
                 ...FONTS.body7,
@@ -273,74 +223,21 @@ function Football() {
           )}
         </div>
       )}
-
-         {groupedByUpcomingLeague && Object.keys(groupedByUpcomingLeague)?.map((leagueName) => (
-        <div key={leagueName}>
+         {upcoming?.map((item, i) => (
+        <div key={i}>
           <p style={{ ...FONTS.body7,backgroundColor: COLORS.lightRed, padding: 5, marginBottom: 10, borderRadius: 5, color: COLORS.black, marginRight: 10 }}>
-            {leagueName}
+            {item?.league}
           </p>
           <div>
-            {groupedByUpcomingLeague[leagueName].map((aa, i) => {
-              return (
-                <div key={i}>
-                  <GameCard id={i} data={aa} />
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      ))}
-      </> : null
-     }
-
-     {
-      selectedStatus === "Finished" ?
-      <>
-       {today?.data?.length > 0 && (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <p style={{ ...FONTS.body6, color: COLORS.gray, margin: "15px 0px" }}>
-          
-          </p>
-          {today?.total > 10 && (
-            <p
-              style={{
-                ...FONTS.body7,
-                color: COLORS.orange,
-                cursor: "pointer",
-                margin: "15px 0px",
-              }}
-              onClick={() =>
-                navigate("/events", {
-                  state: {
-                    events: today,
-                    type: "today",
-                    gameType: "Soccer",
-                  },
-                })
+            {item?.matches?.map((aa, i) => {
+              const payload = {
+                league: item?.league,
+                country: item?.country,
+                ...aa
               }
-            >
-              View more
-            </p>
-          )}
-        </div>
-      )}
-
-         {groupedByTodayLeague && Object.keys(groupedByTodayLeague)?.filter((a, i) => i < 10).map((leagueName) => (
-        <div key={leagueName}>
-          <p style={{ ...FONTS.body7,backgroundColor: COLORS.lightRed, padding: 5, marginBottom: 10, borderRadius: 5, color: COLORS.black, marginRight: 10 }}>
-            {leagueName}
-          </p>
-          <div>
-            {groupedByTodayLeague[leagueName].map((aa, i) => {
               return (
                 <div key={i}>
-                  <GameCard id={i} data={aa} />
+                  <GameCard id={i} data={payload} />
                 </div>
               );
             })}
@@ -350,10 +247,72 @@ function Football() {
       </>: null
      }
 
+     {
+      selectedStatus === "Finished" ?
+      <>
+      {finished?.length > 0 && (
+       <div
+         style={{
+           display: "flex",
+           justifyContent: "space-between",
+           alignItems: "center",
+         }}
+       >
+         <p style={{ ...FONTS.body6, color: COLORS.gray, margin: "15px 0px" }}>
+           
+         </p>
+
+         {finished?.length > 10 && (
+           <p
+             style={{
+               ...FONTS.body7,
+               color: COLORS.orange,
+               cursor: "pointer",
+               margin: "15px 0px",
+             }}
+             onClick={() =>
+               navigate("/events", {
+                 state: {
+                   events: finished,
+                   type: "finished",
+                   gameType: "Soccer",
+                 },
+               })
+             }
+           >
+             View more
+           </p>
+         )}
+       </div>
+     )}
+        {finished?.map((item, i) => (
+       <div key={i}>
+         <p style={{ ...FONTS.body7,backgroundColor: COLORS.lightRed, padding: 5, marginBottom: 10, borderRadius: 5, color: COLORS.black, marginRight: 10 }}>
+           {item?.league}
+         </p>
+         <div>
+           {item?.matches?.map((aa, i) => {
+             const payload = {
+               league: item?.league,
+               country: item?.country,
+               ...aa
+             }
+             return (
+               <div key={i}>
+                 <GameCard id={i} data={payload} />
+               </div>
+             );
+           })}
+         </div>
+       </div>
+     ))}
+     </>: null
+     }
+
       {
         selectedStatus === "Next Day" ?
         <>
-          {tomorrow?.data?.length > 0 && (
+       {tomorrow?.length > 0 && (
         <div
           style={{
             display: "flex",
@@ -362,9 +321,10 @@ function Football() {
           }}
         >
           <p style={{ ...FONTS.body6, color: COLORS.gray, margin: "15px 0px" }}>
-        
+            
           </p>
-          {tomorrow?.total > 10 && (
+
+          {tomorrow?.length > 10 && (
             <p
               style={{
                 ...FONTS.body7,
@@ -387,31 +347,34 @@ function Football() {
           )}
         </div>
       )}
-
-        {groupedByTomorrowLeague && Object.keys(groupedByTomorrowLeague)?.filter((a, i) => i < 10).map((leagueName) => (
-        <div key={leagueName}>
+         {tomorrow?.map((item, i) => (
+        <div key={i}>
           <p style={{ ...FONTS.body7,backgroundColor: COLORS.lightRed, padding: 5, marginBottom: 10, borderRadius: 5, color: COLORS.black, marginRight: 10 }}>
-            {leagueName}
+            {item?.league}
           </p>
           <div>
-            {groupedByTomorrowLeague[leagueName].map((aa, i) => {
+            {item?.matches?.map((aa, i) => {
+              const payload = {
+                league: item?.league,
+                country: item?.country,
+                ...aa
+              }
               return (
                 <div key={i}>
-                  <GameCard id={i} data={aa} />
+                  <GameCard id={i} data={payload} />
                 </div>
               );
             })}
           </div>
         </div>
       ))}
-
-        </>
+      </>
         : null
       }
       {live?.length < 1 &&
-      upcoming?.data?.length < 1 &&
-      today?.data?.length < 1 &&
-      tomorrow?.data?.length < 1 ? (
+      upcoming?.length < 1 &&
+      today?.length < 1 &&
+      tomorrow?.length < 1 ? (
         <EmptyState header="No Game Available for Football" height="30vh" />
       ) : null}
     </div>
