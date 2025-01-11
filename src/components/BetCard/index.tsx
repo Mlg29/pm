b08c05e80ff796a59b39d760b88236b21d858f70
hamskid-id <lawalhamzat27@gmail.com>
2025-01-11 05:00:54 -0800

    updated

--- src/components/BetCard/index.tsx
+++ src/components/BetCard/index.tsx
@@ -1,73 +1,73 @@
-import { COLORS } from "../../utils/colors";
-import { FONTS } from "../../utils/fonts";
-import { FlexDirection } from "../../utils/type";
-import milan from "../../assets/images/millan.svg";
-import roma from "../../assets/images/roma.svg";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import AcceptModal from "../Modals/AcceptModal";
-import AdjustModal from "../Modals/AdjustModal";
+import { COLORS } from '../../utils/colors'
+import { FONTS } from '../../utils/fonts'
+import { FlexDirection } from '../../utils/type'
+import milan from '../../assets/images/millan.svg'
+import roma from '../../assets/images/roma.svg'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import AcceptModal from '../Modals/AcceptModal'
+import AdjustModal from '../Modals/AdjustModal'
 
 const styles = {
   contain: {
     padding: 10,
     border: `1px solid ${COLORS.semiGray}`,
     borderRadius: 10,
-    margin: "1px",
-    width: "100%",
-    marginBottom: 10,
+    margin: '1px',
+    width: '100%',
+    marginBottom: 10
   },
   row: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    margin: "10px 0px",
-    borderBottom: `1px solid ${COLORS.semiGray}`,
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '10px 0px',
+    borderBottom: `1px solid ${COLORS.semiGray}`
   },
   center: {
-    display: "flex",
-    flexDirection: "column" as FlexDirection,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-};
+    display: 'flex',
+    flexDirection: 'column' as FlexDirection,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+}
 
 const BetCard = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const [accept, setAccept] = useState(false);
-  const [reject, setReject] = useState(false);
+  const [accept, setAccept] = useState(false)
+  const [reject, setReject] = useState(false)
 
   return (
     <>
       <div style={{ ...styles.contain }}>
-        <p style={{ ...FONTS.body7, margin: "0px 0px 1rem 0px" }}>
+        <p style={{ ...FONTS.body7, margin: '0px 0px 1rem 0px' }}>
           UEFA - Champions League
         </p>
 
         <div style={{ ...styles.row }}>
           <div style={{ ...styles.center }}>
             <img src={milan} />
-            <h3 style={{ ...FONTS.h6, marginTop: "10px" }}>Milan</h3>
+            <h3 style={{ ...FONTS.h6, marginTop: '10px' }}>Milan</h3>
           </div>
           <div style={{ ...styles.center }}>
-            <p style={{ ...FONTS.body7, marginTop: "10px" }}>10:15 PM</p>
-            <h3 style={{ ...FONTS.h6, marginTop: "5px" }}>₦ 10,000</h3>
+            <p style={{ ...FONTS.body7, marginTop: '10px' }}>10:15 PM</p>
+            <h3 style={{ ...FONTS.h6, marginTop: '5px' }}>₦ 10,000</h3>
           </div>
           <div style={{ ...styles.center }}>
             <img src={roma} />
-            <h3 style={{ ...FONTS.h6, marginTop: "10px" }}>AS Roma</h3>
+            <h3 style={{ ...FONTS.h6, marginTop: '10px' }}>AS Roma</h3>
           </div>
         </div>
 
-        <div style={{ ...styles.row, paddingBottom: "1rem" }}>
+        <div style={{ ...styles.row, paddingBottom: '1rem' }}>
           <div>
-            <p style={{ ...FONTS.body7, marginTop: "10px" }}>@JohnDdon</p>
+            <p style={{ ...FONTS.body7, marginTop: '10px' }}>@JohnDdon</p>
             <p style={{ ...FONTS.body7 }}>Milan Win</p>
           </div>
           <div>
             <p
-              style={{ ...FONTS.body7, marginTop: "10px", textAlign: "right" }}
+              style={{ ...FONTS.body7, marginTop: '10px', textAlign: 'right' }}
             >
               You
             </p>
@@ -75,13 +75,13 @@ const BetCard = () => {
           </div>
         </div>
 
-        <div style={{ ...styles.row, paddingBottom: "0rem", border: "none" }}>
+        <div style={{ ...styles.row, paddingBottom: '0rem', border: 'none' }}>
           <div
             style={{
               backgroundColor: COLORS.primary,
-              width: "48%",
+              width: '48%',
               padding: 10,
-              borderRadius: 10,
+              borderRadius: 10
             }}
             onClick={() => setAccept(true)}
           >
@@ -89,8 +89,8 @@ const BetCard = () => {
               style={{
                 ...FONTS.body7,
                 color: COLORS.white,
-                textAlign: "center",
-                cursor: "pointer",
+                textAlign: 'center',
+                cursor: 'pointer'
               }}
             >
               Accept Bet
@@ -99,9 +99,9 @@ const BetCard = () => {
           <div
             style={{
               backgroundColor: COLORS.cream,
-              width: "48%",
+              width: '48%',
               padding: 10,
-              borderRadius: 10,
+              borderRadius: 10
             }}
             onClick={() => setReject(true)}
           >
@@ -109,8 +109,8 @@ const BetCard = () => {
               style={{
                 ...FONTS.h7,
                 color: COLORS.primary,
-                textAlign: "center",
-                cursor: "pointer",
+                textAlign: 'center',
+                cursor: 'pointer'
               }}
             >
               Adjust Bet
@@ -119,18 +119,11 @@ const BetCard = () => {
         </div>
       </div>
 
-      <AcceptModal 
-        show={accept}
-        handleClose={() => setAccept(false)}
-      />
+      <AcceptModal show={accept} handleClose={() => setAccept(false)} />
 
-
-<AdjustModal 
-        show={reject}
-        handleClose={() => setReject(false)}
-      />
+      <AdjustModal show={reject} handleClose={() => setReject(false)} />
     </>
-  );
-};
+  )
+}
 
-export default BetCard;
+export default BetCard
